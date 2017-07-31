3faecf484068b79edc41d13e18c367749e18f2b3
LaToya Williams <latoyawilliams@LaToyas-MacBook-Pro.local>
2017-07-31 15:39:08 -0700

    A test using expected/valid inputs for the reverseSentence() function is written in tests.js. A test using unexpected/invalid inputs for the reverseSentence() function is written in tests.js. Correct implementation of the reverseSentence() is defined in functions.js. Tests for reverseSentence() are passing.

--- test/test.js
+++ test/test.js
@@ -1,29 +1,53 @@
 const assert = require('chai').assert;
 const getMonth = require('../part-1/functions').getMonth;
 const filterBetween = require('../part-1/functions').filterBetween;
+const reverseSentence = require('../part-1/functions').reverseSentence;
 
-describe('Part-1 Tests', function(){
-describe('getMonth', function(){
-    it('month should return August', function () {
+describe('Part-1 Tests', function() {
+  describe('getMonth', function() {
+    it('month should return August', function() {
       assert.equal(getMonth('2017, 8, 3'), 'August')
     })
 
-    it('month should not return a number', function () {
+    it('month should not return a number', function() {
       assert.notTypeOf(getMonth('2017, 7, 9'), 'number')
     })
   });
 
-  describe('filterBetween', function(){
-      it('filterBetween should return ["dog", "lion", "cow"]', function () {
-        let arr = ["dog", "cat", "zebra", "ape", "lion", "cow"]
-        assert.deepEqual(filterBetween(arr, "chimp", "lobster"), ["dog", "lion", "cow"])
-      })
+  describe('reverseSentence', function() {
+    it('reverseSentence should return "dog. the over jumped cat The"', function() {
+      assert.equal(reverseSentence("The cat jumped over the dog."), "dog. the over jumped cat The")
+    })
 
-      it('filterBetween should return not []', function () {
-        let arr = ["dog", "cat", "zebra", "ape", "lion", "cow"]
-        assert.notDeepEqual(filterBetween(arr, 'act', 'chimpanzee'), [])
-      })
-    });
+    it('reverseSentence should not return a "Hello, world!"', function() {
+      assert.notEqual(reverseSentence("Hello, world!"), "Hello, world!")
+    })
+  });
 
+  describe('filterBetween', function() {
+    it('filterBetween should return ["dog", "lion", "cow"]', function() {
+      let arr = [
+        "dog",
+        "cat",
+        "zebra",
+        "ape",
+        "lion",
+        "cow"
+      ]
+      assert.deepEqual(filterBetween(arr, "chimp", "lobster"), ["dog", "lion", "cow"])
+    })
+
+    it('filterBetween should return not []', function() {
+      let arr = [
+        "dog",
+        "cat",
+        "zebra",
+        "ape",
+        "lion",
+        "cow"
+      ]
+      assert.notDeepEqual(filterBetween(arr, 'act', 'chimpanzee'), [])
+    })
+  });
 
 });
