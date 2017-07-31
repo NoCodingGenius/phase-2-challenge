08fdd719618687d55d4c48996e57c81e4c7e8444
LaToya Williams <platinumtoii@yahoo.com>
2017-07-31 12:27:53 -0700

    A test using expected/valid inputs for the month() function is written in tests.js. A test using unexpected/invalid inputs for the month() function is written in tests.js. Correct implementation of the month() is defined in functions.js. Tests for month() are passing.

---
+++ part-1/functions.js
@@ -0,0 +1,22 @@
+const getMonth = function month(string) {
+  let date = new Date(string);
+  let index = date.getMonth();
+  let months = ["January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"];
+
+  return months[index];
+};
+
+module.exports = {
+  getMonth
+}
