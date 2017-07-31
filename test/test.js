9f1da31c7be5919140110b330fa5a454e78c6560
LaToya Williams <platinumtoii@yahoo.com>
2017-07-31 14:49:43 -0700

    A test using expected/valid inputs for the filterBetween() function is written in tests.js.A test using unexpected/invalid inputs for the filterBetween() function is written in tests.js. Correct implementation of the filterBetween() is defined in functions.js. Tests for filterBetween() are passing.

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 const assert = require('chai').assert;
 const getMonth = require('../part-1/functions').getMonth;
+const filterBetween = require('../part-1/functions').filterBetween;
 
+describe('Part-1 Tests', function(){
 describe('getMonth', function(){
     it('month should return August', function () {
       assert.equal(getMonth('2017, 8, 3'), 'August')
@@ -10,3 +12,18 @@ describe('getMonth', function(){
       assert.notTypeOf(getMonth('2017, 7, 9'), 'number')
     })
   });
+
+  describe('filterBetween', function(){
+      it('filterBetween should return ["dog", "lion", "cow"]', function () {
+        let arr = ["dog", "cat", "zebra", "ape", "lion", "cow"]
+        assert.deepEqual(filterBetween(arr, "chimp", "lobster"), ["dog", "lion", "cow"])
+      })
+
+      it('filterBetween should return not []', function () {
+        let arr = ["dog", "cat", "zebra", "ape", "lion", "cow"]
+        assert.notDeepEqual(filterBetween(arr, 'act', 'chimpanzee'), [])
+      })
+    });
+
+
+});
