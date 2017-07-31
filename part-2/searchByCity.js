5a10d3a745ecefd96e7b2a3e45c19ccf58bc080f
LaToya Williams <latoyawilliams@LaToyas-MacBook-Pro.local>
2017-07-31 13:57:33 -0700

    Beautify code to make clean and readable.

--- part-2/searchByCity.js
+++ part-2/searchByCity.js
@@ -5,19 +5,14 @@ const city = process.argv[2];
 
 cityFinder = function(search) {
   console.log("Finding clients in City " + city + "...");
-  var output = filteredCities.map(function(i){
-    return {
-      "id": i.id,
-      "rep_name": i.rep_name,
-      "company": i.company,
-      "city": i.city,
-      "state": i.state}
+  var output = filteredCities.map(function(i) {
+    return {"id": i.id, "rep_name": i.rep_name, "company": i.company, "city": i.city, "state": i.state}
   });
   console.log(output);
 };
 
-var filteredCities = citiesParsed.filter(function(i){
-  return i.city.toLowerCase().startsWith(city) == true
+var filteredCities = citiesParsed.filter(function(i) {
+  return i.city.toLowerCase().startsWith(city) === true
 })
 
 cityFinder(city);
