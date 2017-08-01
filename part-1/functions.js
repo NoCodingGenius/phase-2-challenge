e151aa0e375c28b93b0ed7f8ba6862c93c0c1582
LaToya Williams <latoyawilliams@LaToyas-MacBook-Pro.local>
2017-08-01 09:47:36 -0700

    Complete writting functions and test for said functions in part-2 of the Phase-2-Challenge/Interview.

--- part-1/functions.js
+++ part-1/functions.js
@@ -1,39 +1,46 @@
 const getMonth = function month(string) {
   let date = new Date(string);
   let index = date.getMonth();
-  let months = ["January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
 
   return months[index];
 };
 
 const reverseSentence = function reverseSentence(string) {
   var arr = string.split(" ").reverse().join(" ")
-  return arr;
-}
+  return arr
+};
+
+const nameProps = function nameProps(obj) {
+  return Object.keys(obj).sort();
+};
 
 const filterBetween = function filterBetween(arr, min, max) {
-    let newArray = []
-    arr.filter(function(string){
-      if (string >= min && string <= max) {
-        newArray.push(string)
-      }
-    })
-    return newArray;
-  };
+  let newArray = []
+  arr.filter(function(string) {
+    if (string >= min && string <= max) {
+      newArray.push(string)
+    }
+  })
+  return newArray;
+};
 
 module.exports = {
   getMonth,
   filterBetween,
-  reverseSentence
+  reverseSentence,
+  nameProps
 }
