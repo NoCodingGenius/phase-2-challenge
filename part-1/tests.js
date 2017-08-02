33880b169069e3c88b45667389b7276723a6af31
LaToya Williams <platinumtoii@yahoo.com>
2017-08-02 10:27:11 -0700

    Rearrange code to ensure it tells a proper story. Give variables practical names that anyone coming in can read and understand your code.

--- part-1/tests.js
+++ part-1/tests.js
@@ -1,5 +1,5 @@
 const assert = require('chai').assert;
-const getMonth = require('../part-1/functions').getMonth;
+const month = require('../part-1/functions').month;
 const reverseSentence = require('../part-1/functions').reverseSentence;
 const nameProps = require('../part-1/functions').nameProps;
 const filterBetween = require('../part-1/functions').filterBetween;
@@ -7,11 +7,11 @@ const filterBetween = require('../part-1/functions').filterBetween;
 describe('Part-1 Tests', function() {
   describe('getMonth', function() {
     it('month should return August', function() {
-      assert.equal(getMonth('2017, 8, 3'), 'August')
+      assert.equal(month('2017, 8, 3'), 'August')
     })
 
     it('month should not return a number', function() {
-      assert.notTypeOf(getMonth('2017, 7, 9'), 'number')
+      assert.notTypeOf(month('2017, 7, 9'), 'number')
     })
   });
 
@@ -42,7 +42,7 @@ describe('Part-1 Tests', function() {
 
   describe('filterBetween', function() {
     it('filterBetween should return ["dog", "lion", "cow"]', function() {
-      let arr = [
+      let animals = [
         "dog",
         "cat",
         "zebra",
@@ -50,7 +50,7 @@ describe('Part-1 Tests', function() {
         "lion",
         "cow"
       ]
-      assert.deepEqual(filterBetween(arr, "chimp", "lobster"), ["dog", "lion", "cow"])
+      assert.deepEqual(filterBetween(animals, "chimp", "lobster"), ["dog", "lion", "cow"])
     })
 
     it('filterBetween should not return an empty array []', function() {
