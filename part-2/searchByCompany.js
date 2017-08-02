1a2a08d74c8617a20caa48bff7cb33a863e06e3b
LaToya Williams <latoyawilliams@LaToyas-MacBook-Pro.local>
2017-08-02 09:20:31 -0700

    Rearrange code to ensure it tells a proper story. Give variables practical names that anyone coming in can read and understand your code.

--- part-2/searchByCompany.js
+++ part-2/searchByCompany.js
@@ -1,18 +1,24 @@
 const fs = require('fs');
 const companies = fs.readFileSync('./clients.json', 'utf-8'); //or .toString
 const companiesParsed = JSON.parse(companies);
-const company = process.argv[2];
+const companySearch = process.argv[2];
 
-const companyFinder = function(search) {
-  console.log("Finding companies with name " + company + "...");
-  var output = filteredCompanies.map(function(clients) {
-    return {"id": clients.id, "company": clients.company, "phone": clients.phone};
+const companyInfoFinder = function(search) {
+
+  console.log("Finding companies with name " + companySearch + "...");
+
+  const filteredCompanies = companiesParsed.filter(function(clients) {
+    return clients.company.toLowerCase().startsWith(companySearch);
+  })
+
+  const searchResults = filteredCompanies.map(function(clients) {
+    return {
+      "id": clients.id,
+      "company": clients.company,
+      "phone": clients.phone
+    }
   });
-  console.log(output);
+  console.log(searchResults);
 };
 
-const filteredCompanies = companiesParsed.filter(function(clients) {
-  return clients.company.toLowerCase().startsWith(company);
-})
-
-companyFinder(company);
+companyInfoFinder(companySearch);
