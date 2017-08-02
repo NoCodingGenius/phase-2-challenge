3106189334650e2b0ab42b9a3cfd7e5b03b8d1ba
LaToya Williams <platinumtoii@yahoo.com>
2017-08-02 09:20:31 -0700

    Rearrange code to ensure it tells a proper story. Give variables practical names that anyone coming in can read and understand your code.

--- part-2/searchByCity.js
+++ part-2/searchByCity.js
@@ -1,18 +1,26 @@
 const fs = require('fs');
 const cities = fs.readFileSync('./clients.json', 'utf-8');
 const citiesParsed = JSON.parse(cities);
-const city = process.argv[2];
+const citySearch = process.argv[2];
 
-const cityFinder = function(search) {
-  console.log("Finding clients in City " + city + "...");
-  var output = filteredCities.map(function(clients) {
-    return {"id": clients.id, "rep_name": clients.rep_name, "company": clients.company, "city": clients.city, "state": clients.state}
+const cityInfoFinder = function(search) {
+
+  console.log("Finding clients in City " + citySearch + "...");
+
+  const filteredCities = citiesParsed.filter(function(clients) {
+    return clients.city.toLowerCase().startsWith(citySearch);
+  })
+
+  const searchResults = filteredCities.map(function(clients) {
+    return {
+      "id": clients.id,
+      "rep_name": clients.rep_name,
+      "company": clients.company,
+      "city": clients.city,
+      "state": clients.state
+    }
   });
-  console.log(output);
+  console.log(searchResults);
 };
 
-const filteredCities = citiesParsed.filter(function(clients) {
-  return clients.city.toLowerCase().startsWith(city);
-})
-
-cityFinder(city);
+cityInfoFinder(citySearch);
